3f177113bfbcc4ea4a36fe856f4e7ef759f23505
sabrina170 <sabrina.pomajulca@tecsup.edu.pe>
2019-05-10 16:19:58 -0500

    First Commit

---
+++ src/main/java/com/tecsup/CPS_20191_G7_1/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.CPS_20191_G7_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
